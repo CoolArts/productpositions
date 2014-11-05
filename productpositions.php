5044745b3f07e1f3998b12fc5119ed4e68e0a489
CoolArts <github@coolarts.us>
2014-11-05 05:38:14 +0100

    Working
    On phase before production and opimizing

--- productpositions.php
+++ productpositions.php
@@ -21,7 +21,6 @@ class ProductPositions extends Module {
     public function install() {
         return parent::install();
     }
-    
     public function uninstall() {
         return parent::uninstall();
     }
@@ -29,48 +28,12 @@ class ProductPositions extends Module {
     public function getContent() {
         if (Tools::isSubmit('OLD_POSITIONS')) {
             $this->flushProducts();
+            $this->updatePositions();
         }
     
         if ($this->context->controller->controller_type == 'admin')
             return $this->renderForm();
     }
-    
-    private function flushProducts () {
-        $query = 'SELECT `id_product`, `id_category_default` FROM `'._DB_PREFIX_.'product`';
-        
-        if (!$data = $this->Db->ExecuteS($query)) return;
-        if (count($data) < 1) return;
-        
-        $root = $this->getRootCategories();
-        
-        foreach ($data as $product) {
-            if (in_array($product['id_category_default'], $root)) continue;
-            
-            $parents = Category::getParentsCategories($this->context->language->id, $product['id_category_default']);
-            
-            if (count($parents) > 0) {
-                foreach ($parents as $parent) {
-                    $this->Db->insert(
-                        'category_product',
-                        array(
-                            'id_product' => $product['id_product'],
-                            'id_category' => $parent['id_category'],
-                            'position' => 0,
-                        )
-                    );
-                }
-            }
-        }
-    }
-    
-    private function getRootCategories ($root = true) {
-        $roots = 'SELECT `id_category` FROM category WHERE is_root_category = '.$root;
-        $root = $this->Db->ExecuteS($roots);
-        
-        foreach ($root as $k => $v) $root[$k] = $v['id_category'];
-        return $root;
-    }
-    
     private function renderForm() {
         $fields_form = array(
             'form' => array(
@@ -122,5 +85,76 @@ class ProductPositions extends Module {
         );
         
         return $helper->generateForm(array($fields_form));
+    }
+    
+    private function flushProducts () {
+        $query = 'SELECT `id_product`, `id_category_default` FROM `'._DB_PREFIX_.'product`';
+        
+        if (!$data = $this->Db->ExecuteS($query)) return;
+        if (count($data) < 1) return;
+        
+        $root = $this->getRootCategories();
+        
+        //Add position switch functionality
+        $max = (int)$this->getMaxPosition()+10;
+        
+        foreach ($data as $product) {
+            if (in_array($product['id_category_default'], $root)) continue;
+            
+            // The method Category::getParentsCategories is overriden to accept and id
+            $parents = Category::getParentsCategories($this->context->language->id, $product['id_category_default']);
+            
+            if (count($parents) > 0) {
+                foreach ($parents as $parent) {
+                    $this->Db->insert(
+                        'category_product',
+                        array(
+                            'id_product' => $product['id_product'],
+                            'id_category' => $parent['id_category'],
+                            //Add position switch functionality
+                            'position' => $max,
+                        )
+                    );
+                }
+            }
+        }
+        
+        $this->updatePositions();
+    }
+    private function getMaxPosition () {
+        $max = 'SELECT MAX(`position`) AS `max` FROM `'._DB_PREFIX_.'category_product`';
+        $maxPos = $this->Db->ExecuteS($max);
+        return $maxPos[0]['max'];
+    }
+    private function updatePositions () {
+        $categories = $this->getRootCategories(0);
+        
+        //Add position switch functionality
+        foreach ($categories as $category) {
+            $query = '
+                SELECT cp.* FROM `'._DB_PREFIX_.'category_product` cp
+                INNER JOIN `'._DB_PREFIX_.'product` p ON p.`id_product` = cp.`id_product`
+                WHERE cp.`id_category` = '.$category.'
+                ORDER BY p.`active` DESC, cp.`position` ASC
+            ';
+            
+            $positions = $this->Db->ExecuteS($query);
+            $counter = 0;
+            foreach ($positions as $pos) {
+                extract($pos);
+                
+                $queryb = 'UPDATE `category_product` SET `position` = '.$counter.' WHERE `id_category` = '.$id_category.' AND `id_product` = '.$id_product;
+                
+                $this->Db->ExecuteS($queryb);
+                $counter++;
+            }
+        }
+    }
+    private function getRootCategories ($root = 1) {
+        $roots = 'SELECT `id_category` FROM category WHERE is_root_category = '.$root;
+        $root = $this->Db->ExecuteS($roots);
+        
+        foreach ($root as $k => $v) $root[$k] = $v['id_category'];
+        return $root;
     }
 }
